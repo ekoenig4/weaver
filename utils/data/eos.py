8e4f19c134ab4cb1978801d8d39f8e72b7b238fc
Evan Koenig <ekoenig4@wisc.edu>
2022-09-28 15:46:53 -0500

    update eos commands

--- utils/data/eos.py
+++ utils/data/eos.py
@@ -8,7 +8,19 @@ class eos:
     url = "root://cmseos.fnal.gov/"
 
     @staticmethod
-    def ls(path, with_path=False):
+    def ls_with_uscms(path, with_path):
+        cmd = ['ls', '/eos/uscms'+path]
+        stdout = subprocess.run(
+            [' '.join(cmd)], shell=True, capture_output=True).stdout.decode("utf-8")
+        dirlist = stdout.strip().split('\n')
+
+        if with_path:
+            return [ d.replace('/eos/uscms','') for d in dirlist ]
+        else:
+            return [ os.path.basename(d) for d in dirlist ]
+        
+    @staticmethod
+    def ls_with_eos(path, with_path=False):
         cmd = ['eos', eos.url, 'ls', path]
         stdout = subprocess.run(
             [' '.join(cmd)], shell=True, capture_output=True).stdout.decode("utf-8")
@@ -20,6 +32,15 @@ class eos:
         return dirlist
 
     @staticmethod
+    def ls(path, with_path=False, with_eos=False):
+        if with_eos: 
+            dirlist = eos.ls_with_eos(path, with_path)
+        else:
+            dirlist = eos.ls_with_uscms(path, with_path)
+
+        return dirlist
+
+    @staticmethod
     def exists(path):
         cmd = ['eos', eos.url, 'ls', path]
         stdout = subprocess.run(
@@ -28,8 +49,8 @@ class eos:
         return any(stdout)
 
     @staticmethod
-    def copy(src, dest):
-        cmd = ['xrdcp', eos.fullpath(src), dest]
+    def copy(src, dest,):
+        cmd = ['xrdcp','-f', src, dest]
         return subprocess.run(
             [' '.join(cmd)], shell=True)
 
@@ -50,7 +71,13 @@ def copy(src, dest):
     if os.path.exists(src):
         return shutil.copy2(src, dest)
     if eos.exists(src):
-        return eos.copy(src, dest)
+        return copy_from_eos(src, dest)
+
+def copy_to_eos(src, dest):
+    return eos.copy(src, eos.fullpath(dest))
+
+def copy_from_eos(src, dest):
+    return eos.copy(eos.fullpath(src), dest)
 
 def fullpath(path):
     if os.path.exists(path):
@@ -58,3 +85,6 @@ def fullpath(path):
     if eos.exists(path):
         return eos.fullpath(path)
     return path
+
+def remove_eos(path):
+    return path.replace('/eos/uscms', '')
